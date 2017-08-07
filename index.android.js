4330a7d720cfd805561978f3e8c50c7abe5e06c1
naveedaheer <naveedaheer@gmail.com>
2017-08-07 07:43:32 +0500

    integrating react-native-maps

--- index.android.js
+++ index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,43 +5,72 @@ import {
   Text,
   View
 } from 'react-native';
+import RunInfo from './src/components/runInfo';
+import RunInfoNumeric from './src/components/runInfoNumeric';
+import sharedStyles from './src/styles/sharedStyle';
+import MapView from 'react-native-maps';
+
+
+const styles = StyleSheet.create({
+  infoWrapper: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    right: 0,
+    flexDirection: 'row',
+    flex: 1
+  },
+  mapView: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
 
 export default class MapTrackingApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    setInterval(() => {
+      this.distanceInfo.setState({ value: Math.random() * 100 });
+      this.speedInfo.setState({ value: Math.random() * 15 });
+      this.directionInfo.setState({ value: this.directionInfo.state == 'N' ? 'NW' : 'N' });
+    }, 1000);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={{ flex: 1 }}>
+        <MapView style={styles.mapView}
+          /*initialRegion={{
+            latitude: 24.53,
+            longitude: 67.00,
+            latitudeDelta: 0.02,
+            longitudeDelta: 0.02,
+          }}*/
+        />
+        <View style={styles.infoWrapper} >
+          <RunInfoNumeric
+            title='Distance'
+            unit='km'
+            ref={(info) => this.distanceInfo = info}
+          />
+          <RunInfoNumeric
+            title='Speed'
+            unit='km/h'
+            ref={(info) => this.speedInfo = info}
+          />
+          <RunInfo
+            title='Direction'
+            value='NE'
+            ref={(info) => this.directionInfo = info}
+          />
+        </View>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('MapTrackingApp', () => MapTrackingApp);
