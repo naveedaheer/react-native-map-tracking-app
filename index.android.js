451cf4de40540f7a2fe1d84554ee6674edb703ad
naveedaheer <naveedaheer@gmail.com>
2017-08-08 10:40:18 +0500

    marker added as tracing line

--- index.android.js
+++ index.android.js
@@ -21,43 +21,89 @@ const styles = StyleSheet.create({
     flex: 1
   },
   map: {
-        ...StyleSheet.absoluteFillObject,
-    // position: 'absolute',
-    // top: 0,
-    // left: 0,
-    // right: 0,
-    // bottom: 0,
+    ...StyleSheet.absoluteFillObject,
   }
 });
+
+let id = 0;
 
 export default class MapTrackingApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {
 
+    let watchId = navigator.geolocation.watchPosition((postion) => {
+      this.speedInfo.setState({value:postion.coords.speed});
+      this.setState({
+        markers: [
+          ...this.state.markers,
+          {
+            coordinate: postion.coords,
+            key: id++
+          }
+        ]
+      }, null, { distanceFilter: 10 }
+      );
+    });
+
+
+    this.state = {
+      markers: [],
+      watchId
     };
 
-    setInterval(() => {
-      this.distanceInfo.setState({ value: Math.random() * 100 });
-      this.speedInfo.setState({ value: Math.random() * 15 });
-      this.directionInfo.setState({ value: this.directionInfo.state == 'N' ? 'NW' : 'N' });
-    }, 1000);
+    // setInterval(() => {
+    //   this.distanceInfo.setState({ value: Math.random() * 100 });
+    //   this.speedInfo.setState({ value: Math.random() * 15 });
+    //   this.directionInfo.setState({ value: this.directionInfo.state == 'N' ? 'NW' : 'N' });
+    // }, 1000);
+  }
+
+componentWillUnmount(){
+  navigator.geolocation.stopWatch(this.state.watchId);
+}
+
+  addMarker(region) {
+    let now = (new Date).getTime();
+    if (this.state.lastAddedMarker > (now - 5000)) {
+      return;
+    }
+    this.setState({
+      markers: [
+        ...this.state.markers, {
+          coordinate: region,
+          key: id++
+        }
+      ],
+      lastAddedMarker: now
+    });
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <MapView style={styles.map}
+          showsUserLocation={true}
+        //  followsUserLocation={true}
+          showsMyLocationButton={true}
+          zoomEnabled={true}
           initialRegion={{
             latitude: 24.8615,
             longitude: 67.0099,
-            latitudeDelta: 0.06,
-            longitudeDelta: 0.06,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.001,
           }}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-          zoomEnabled={true}
-        />
+          onRegionChange={(region) => this.addMarker(region)}
+
+        >
+        <MapView.Polyline
+              coordinates={this.state.markers.map((marker) => marker.coordinate)}
+              strokeWidth={5}
+            />
+          {/*{this.state.markers.map((marker) => (
+            <MapView.Marker coordinate={marker.coordinate} key={marker.key} />
+          ))}*/}
+        </MapView>
+
         <View style={styles.infoWrapper} >
           <RunInfoNumeric
             title='Distance'
